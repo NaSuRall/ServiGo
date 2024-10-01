035aad4f3640a4c604039db4fcfd126bddc5e33c
Romain Poulain <romain.poulain15@gmail.com>
2024-10-01 09:06:38 +0200

    V5

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// Sélectionne les éléments
+const video = document.getElementById('intro-video');
+const skipButton = document.getElementById('skip-button');
+const introContainer = document.getElementById('intro-container');
+const mainContent = document.getElementById('main-content');
+
+
+function hideIntro() {
+    introContainer.style.opacity = '0'; 
+    setTimeout(() => {
+        introContainer.style.display = 'none';  
+        mainContent.style.display = 'block';    
+    }, 1000); 
+}
+
+setTimeout(hideIntro, 3000);
